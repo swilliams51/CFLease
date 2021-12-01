6f5606e87bf64f650bdd7969b518f33da1ecff0d
steve williams <sjwilliams51@gmail.com>
2021-12-01 10:56:00 -0500

    Initial Commit

---
+++ CFLease Beta 3.0/CFLease_Beta_3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  CFLease_Beta_3_0App.swift
+//  CFLease Beta 3.0
+//
+//  Created by Steven Williams on 12/1/21.
+//
+
+import SwiftUI
+
+@main
+struct CFLease_Beta_3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CFLease Beta 3.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CFLease Beta 3.0
+//
+//  Created by Steven Williams on 12/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
